f743936bcb1ca7033e7bfec37a9e09542d397d54
ddzibela <d.dzibela0000@gmail.com>
2022-10-17 21:30:07 +0200

    Change DAL integration tests to propper project type

--- DameChales/DameChales.API.DAL.IntegrationTests/IDatabaseFixture.cs
+++ DameChales/DameChales.API.DAL.IntegrationTests/IDatabaseFixture.cs
@@ -1,19 +1,11 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using DameChales.API.DAL.Common.Entities;
-//using DameChales.API.DAL.Common.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using DameChales.API.DAL.Common.Entities;
+using DameChales.API.DAL.Common.Repositories;
 
-//namespace DameChales.API.DAL.IntegrationTests;
+namespace DameChales.API.DAL.IntegrationTests;
 
-//public interface IDatabaseFixture
-//{
-//    FoodAmountEntity? GetFoodAmountDirectly(Guid foodAmountId);
-//    OrderEntity? GetOrderDirectly(Guid orderId);
-//    RestaurantEntity? GetRestaurantDirectly(Guid restaurantId);
-//    IRestaurantRepository GetRestaurantRepository();
-//    IList<Guid> FoodGuids {get;};
-//    IList<Guid> FoodAmountGuids{get;};
-//    IList<Guid> OrderGuids{ get; };
-//    IList<Guid> RestaurantGuids { get; };
-//}
+public interface IDatabaseFixture
+{
 
+}
--- DameChales/DameChales.API.DAL.IntegrationTests/InMemoryDatabaseFixture.cs
+++ DameChales/DameChales.API.DAL.IntegrationTests/InMemoryDatabaseFixture.cs
@@ -1,17 +1,9 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using DameChales.API.DAL.Common.Entities;
-//using DameChales.API.DAL.Common.Repositories;
-//using DameChales.API.DAL.Memory;
-//using DameChales.API.DAL.Memory.Repositories;
-//using DameChales.Common.Enums;
-//using DameChales.API.DAL.IntegrationTests;
-//using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace CookBook.API.DAL.IntegrationTests;
+namespace DameChales.API.DAL.IntegrationTests;
 
-//public class InMemoryDatabaseFixture : IDatabaseFixture
-//{
-
-//}
+public class InMemoryDatabaseFixture : IDatabaseFixture
+{
+}
