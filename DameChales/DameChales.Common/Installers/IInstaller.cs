f679336530ef3275149a99ea1140d248ec49ff8e
ddzibela <d.dzibela0000@gmail.com>
2022-10-17 20:49:06 +0200

    Copied common from cookbook

---
+++ DameChales/DameChales.Common/Installers/IInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DameChales.Common.Installers
+{
+    public interface IInstaller
+    {
+        void Install(IServiceCollection serviceCollection);
+    }
+}
