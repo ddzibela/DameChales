4f532639a547fda96ea4e08571a3afdbb79fecd2
martinkubicka <martinkubicka22@gmail.com>
2022-10-18 10:25:50 +0200

    create facades

---
+++ DameChales/DameChales.API.BL/Facades/IFoodFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using DameChales.Common.BL.Facades;
+using DameChales.Common.Models;
+
+namespace DameChales.API.BL.Facades
+{
+    public interface IFoodFacade : IAppFacade
+    {
+        List<FoodListModel> GetAll();
+        FoodDetailModel? GetById(Guid id);
+
+        // TODO ziskanie jedal pre restauraciu
+        // TODO ziskanie jedla byName atd..
+        Guid CreateOrUpdate(FoodDetailModel foodModel);
+        Guid Create(FoodDetailModel foodModel);
+        Guid? Update(FoodDetailModel foodModel);
+        void Delete(Guid id);
+    }
+}
