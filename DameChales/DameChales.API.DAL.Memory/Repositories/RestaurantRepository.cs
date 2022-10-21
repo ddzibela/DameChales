8bd745cb6c17838569efb017913767230b00d64f
ddzibela <d.dzibela0000@gmail.com>
2022-10-21 15:23:49 +0200

    Add GetBy* methods from discord

--- DameChales/DameChales.API.DAL.Memory/Repositories/RestaurantRepository.cs
+++ DameChales/DameChales.API.DAL.Memory/Repositories/RestaurantRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using AutoMapper;
 using DameChales.API.DAL.Common.Entities;
+using DameChales.API.DAL.Common.Entities.Interfaces;
 using DameChales.API.DAL.Common.Repositories;
 
 namespace DameChales.API.DAL.Memory.Repositories
@@ -13,13 +15,15 @@ namespace DameChales.API.DAL.Memory.Repositories
         private readonly IList<OrderEntity> orders;
         private readonly IList<FoodEntity> foods;
         private readonly IList<FoodAmountEntity> foodAmounts;
+        private readonly IMapper mapper;
 
-        public RestaurantRepository(Storage storage)
+        public RestaurantRepository(Storage storage, IMapper mapper)
         {
             restaurants = storage.Restaurants;
             orders = storage.Orders;
             foods = storage.Foods;
             foodAmounts = storage.FoodAmounts;
+            this.mapper = mapper;
         }
 
         public IList<RestaurantEntity> GetAll()
@@ -32,6 +36,23 @@ namespace DameChales.API.DAL.Memory.Repositories
             return restaurants.SingleOrDefault(e => e.Id == id);
         }
 
+        RestaurantEntity? GetByFoodId(Guid id)
+        {
+            return restaurants.SingleOrDefault(e => e.Foods.Any(food => food.Id == id));
+        }
+
+        IList<RestaurantEntity> GetByName(string name)
+        {
+            var nameRegex = new Regex(name);
+            return restaurants.Where(e => nameRegex.IsMatch(e.Name)).ToList();
+        }
+
+        IList<RestaurantEntity> GetByAddress(string address)
+        {
+            var addressRegex = new Regex(address);
+            return restaurants.Where(e => addressRegex.IsMatch(e.Address)).ToList();
+        }
+
         public Guid Insert(RestaurantEntity restaurant)
         {
             restaurants.Add(restaurant);
@@ -40,7 +61,13 @@ namespace DameChales.API.DAL.Memory.Repositories
 
         public Guid? Update(RestaurantEntity restaurant)
         {
+            var restaurantExisting = foods.SingleOrDefault(e => e.Id == restaurant.Id);
+            if (restaurantExisting != null)
+            {
+                mapper.Map(restaurant, restaurantExisting);
+            }
 
+            return restaurantExisting?.Id;
         }
 
         public void Remove(Guid id)
@@ -55,6 +82,13 @@ namespace DameChales.API.DAL.Memory.Repositories
                     foodAmounts.Remove(amount);
                 }
                 orders.Remove(order);
+            }
+
+            var foodsToRemove = foods.Where(e => e.RestaurantGuid == id).ToList();
+
+            foreach(var food in foodsToRemove)
+            {
+                foods.Remove(food);
             }
 
             var restaurantToRemove = restaurants.Single(e => e.Id == id);
