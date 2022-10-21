83da023a4745cf4fe1c4e59a21787840c34f99e2
xmacek27 <matej.macek@email.cz>
2022-10-21 15:44:04 +0200

    Add IDatabaseFixture

--- DameChales/DameChales.API.DAL.IntegrationTests/IDatabaseFixture.cs
+++ DameChales/DameChales.API.DAL.IntegrationTests/IDatabaseFixture.cs
@@ -7,5 +7,24 @@ namespace DameChales.API.DAL.IntegrationTests;
 
 public interface IDatabaseFixture
 {
+    //todo change to our solution
+    //restaurat-recipe
+    //ingredient-order
+
+    FoodAmountEntity? GetFoodAmountDirectly(Guid foodAmountEntity);
+    FoodEntity? GetFoodRepository();
+    OrderEntity? GetOrderDirectly(Guid orderId);
+    RestaurantEntity? GetRestaurantDirectly(Guid restaurantId);
+
+    /*
+    IngredientAmountEntity? GetIngredientAmountDirectly(Guid ingredientAmountId);
+    RecipeEntity? GetRecipeDirectly(Guid recipeId);
+    IRecipeRepository GetRecipeRepository();
+    */
+
+    IList<Guid> foodGuids { get; }
+    IList<Guid> foodAmountGuids { get; }
+    IList<Guid> RecipeGuids { get; }
+    IList<Guid> restaurantGuids { get; }
 
 }
