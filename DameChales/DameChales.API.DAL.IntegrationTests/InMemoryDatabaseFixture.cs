50a6da180d0296a1aec53d321738be1be704fa90
xmacek27 <matej.macek@email.cz>
2022-10-21 22:47:30 +0200

    Edit InMemoryDatabaseFixture

--- DameChales/DameChales.API.DAL.IntegrationTests/InMemoryDatabaseFixture.cs
+++ DameChales/DameChales.API.DAL.IntegrationTests/InMemoryDatabaseFixture.cs
@@ -49,33 +49,9 @@ public class InMemoryDatabaseFixture : IDatabaseFixture
 
     public IRestaurantRepository GetRestaurantRepository()
     {
-        return new RestaurantRepository(inMemoryStorage.Value);
+        return new RestaurantRepository(inMemoryStorage.Value,);
     }
 
-
-
-
-    /*
-    public IRecipeRepository GetRecipeRepository()
-    {
-        return new RecipeRepository(inMemoryStorage.Value);
-    }
-    */
-  
-
-    /*
-    public IList<Guid> IngredientGuids { get; } = new List<Guid>
-    {
-        new("df935095-8709-4040-a2bb-b6f97cb416dc"), new("23b3902d-7d4f-4213-9cf0-112348f56238")
-    };
-
-    public IList<Guid> IngredientAmountGuids { get; } = new List<Guid>
-    {
-        new("0d4fa150-ad80-4d46-a511-4c666166ec5e"), new("87833e66-05ba-4d6b-900b-fe5ace88dbd8")
-    };
-
-    public IList<Guid> RecipeGuids { get; } = new List<Guid> { new("fabde0cd-eefe-443f-baf6-3d96cc2cbf2e") };
-    */
         public IList<Guid> foodGuids = new List<Guid>
         {
             new ("df935025-8709-4040-a2bb-b6f97cb416dc"),
@@ -118,9 +94,7 @@ public class InMemoryDatabaseFixture : IDatabaseFixture
     //todo add restaurants , food and orders 
     private void SeedStorage(Storage storage)
     {
-        {
-
-            Foods.Add(new FoodEntity()
+            storage.Foods.Add(new FoodEntity()
             {
                 Id = foodGuids[0],
                 Name = "orechy",
@@ -131,7 +105,7 @@ public class InMemoryDatabaseFixture : IDatabaseFixture
                 alergens = new HashSet<Alergens>() { Alergens.Nuts }
             });
 
-            Foods.Add(new FoodEntity
+            storage.Foods.Add(new FoodEntity
             {
                 Id = foodGuids[1],
                 Name = "Slehacka",
@@ -141,41 +115,34 @@ public class InMemoryDatabaseFixture : IDatabaseFixture
                 RestaurantGuid = restaurantGuids[0],
                 alergens = new HashSet<Alergens>() { Alergens.Milk }
             });
-        }
-   
-        private void SeedFoodAmounts()
-        {
-            FoodAmounts.Add(new FoodAmountEntity
+        
+            storage.FoodAmounts.Add(new FoodAmountEntity
             {
                 Id = foodAmountGuids[0],
                 OrderGuid = orderGuids[0],
                 Amount = 1,
                 Note = "poznamka"
-    });
+            });
 
-            FoodAmounts.Add(new FoodAmountEntity
+            storage.FoodAmounts.Add(new FoodAmountEntity
             {
                 Id = foodAmountGuids[1],
                 OrderGuid = orderGuids[0],
                 Amount = 2
             });
-        }
 
-        private void SeedOrders()
-        {
-            Orders.Add(new OrderEntity
+            storage.Orders.Add(new OrderEntity
             {
+                Name = "Peter Parker",
                 Id = orderGuids[0],
                 RestaurantGuid = restaurantGuids[0],
                 DeliveryTime = TimeSpan.FromMinutes(15),
                 Note = "Poznamka k objednavce.",
                 Status = OrderStatus.Accepted
             });
-        }
+        
 
-        private void SeedRestaurants()
-        {
-            Restaurants.Add(new RestaurantEntity
+            storage.Restaurants.Add(new RestaurantEntity
             {
                 Id = restaurantGuids[0],
                 Name = "SkvelaRestaurace",
@@ -183,8 +150,6 @@ public class InMemoryDatabaseFixture : IDatabaseFixture
                 LogoURL= "https://m.facebook.com/eggotruckbrno/",
                 Address= "Dvořákova 12, Brno, Czech Republic",
                 GPSCoordinates= "49.195942, 16.611404"
-
             });
-        }
     }
 }
