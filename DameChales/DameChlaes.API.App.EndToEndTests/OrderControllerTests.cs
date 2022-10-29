42c1366043be423106b7c7a4ed03a3178661bcb7
xmacek27 <matej.macek@email.cz>
2022-10-29 12:37:15 +0200

    Add Tests End To End

--- DameChales/DameChlaes.API.App.EndToEndTests/OrderControllerTests.cs
+++ DameChales/DameChlaes.API.App.EndToEndTests/OrderControllerTests.cs
@@ -55,6 +55,131 @@ namespace DameChales.API.App.EndToEndTests
             Assert.NotEmpty(foods);
         }
 
+        [Fact]
+        public async Task GetByIdFood()
+        {
+            var response = await client.Value.GetAsync("/api/food/96103111-393b-46b8-8b4f-ec82212cffbf");
+
+            response.EnsureSuccessStatusCode();
+
+            var foods = await response.Content.ReadFromJsonAsync<FoodDetailModel>();
+            Assert.NotNull(foods);
+        }
+
+        [Fact]
+        public async Task GetApiFoodRestaurantId()
+        {
+            var response = await client.Value.GetAsync("/api/food/restaurant/96103111-393b-46b8-8b4f-ec82212cffbf");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetApiFoodNameRegex()
+        {
+            var response = await client.Value.GetAsync("/api/food/name/Vajicka%20s%20orechy");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetApiFoodNoAlergens()
+        {
+            var response = await client.Value.GetAsync("/api/food/restaurant/96103111-393b-46b8-8b4f-ec82212cffbf/noalergens/1_2_3");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        //orders tests
+
+        [Fact]
+        public async Task GetByIdOrder()
+        {
+            var response = await client.Value.GetAsync("/api/order/e184748d-b151-4129-83f9-f2ac2486fa55");
+
+            response.EnsureSuccessStatusCode();
+
+            var orders = await response.Content.ReadFromJsonAsync<OrderListModel>();
+            Assert.NotNull(orders);
+        }
+
+        [Fact]
+        public async Task GetByIdOrderFood()
+        {
+            var response = await client.Value.GetAsync("/api/order/food/e184748d-b151-4129-83f9-f2ac2486fa55");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByIdOrderRestaurant()
+        {
+            var response = await client.Value.GetAsync("/api/order/restaurant/96103111-393b-46b8-8b4f-ec82212cffbf");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByIdOrderRestaurantStatus()
+        {
+            var response = await client.Value.GetAsync("/api/order/restaurant/96103111-393b-46b8-8b4f-ec82212cffbf/status/1");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByRestaurantId()
+        {
+            var response = await client.Value.GetAsync("/api/restaurant/75970373-0afa-4c9b-9bc3-2655f3c1efe0");
+
+            response.EnsureSuccessStatusCode();
+
+            var restaurants = await response.Content.ReadFromJsonAsync<RestaurantListModel>();
+            Assert.NotNull(restaurants);
+        }
+
+        [Fact]
+        public async Task GetByRestaurantIdRegex()
+        {
+            var response = await client.Value.GetAsync("/api/restaurant/name/SkvelaRestaurace");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByAdressIdRegex()
+        {
+            var response = await client.Value.GetAsync("/api/restaurant/address/Dvo%C5%99%C3%A1kova%2012%2C%20Brno%2C%20Czech%20Republic");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetByIdRestaurantEarnings()
+        {
+            var response = await client.Value.GetAsync("/api/restaurant/food/96103111-393b-46b8-8b4f-ec82212cffbf");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.NotNull(response.StatusCode);
+        }
+
         public async ValueTask DisposeAsync()
         {
             await application.DisposeAsync();
