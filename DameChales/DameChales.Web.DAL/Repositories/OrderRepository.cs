ffca933a59913d0181c8e50ed8056151b714ea33
martinkubicka <martinkubicka22@gmail.com>
2022-11-09 13:39:15 +0100

    create web.dal

---
+++ DameChales/DameChales.Web.DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using DameChales.Common.Models;
+
+namespace DameChales.Web.DAL.Repositories
+{
+    public class OrderRepository : RepositoryBase<OrderDetailModel>
+    {
+        public override string TableName { get; } = "orders";
+
+        public OrderRepository(LocalDb localDb)
+            : base(localDb)
+        {
+        }
+    }
+}
