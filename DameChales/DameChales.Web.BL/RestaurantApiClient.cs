b023bad3a37214f6d697205acff8f195ce960a19
martinkubicka <martinkubicka22@gmail.com>
2022-11-09 13:55:25 +0100

    create web.BL

---
+++ DameChales/DameChales.Web.BL/RestaurantApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace DameChales.Web.BL;
+
+public partial class RestaurantApiClient
+{
+    public RestaurantApiClient(HttpClient httpClient, string baseUrl)
+        : this(httpClient)
+    {
+        BaseUrl = baseUrl;
+    }
+}
