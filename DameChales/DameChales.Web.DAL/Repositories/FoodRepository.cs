194d0ca8de254f6572bc1a47e50ff09fa3f0c973
martinkubicka <martinkubicka22@gmail.com>
2022-11-09 13:39:15 +0100

    create web.dal

---
+++ DameChales/DameChales.Web.DAL/Repositories/FoodRepository.cs
@@ -0,0 +1,14 @@
+﻿using DameChales.Common.Models;
+
+namespace DameChales.Web.DAL.Repositories
+{
+    public class FoodRepository : RepositoryBase<FoodDetailModel>
+    {
+        public override string TableName { get; } = "foods";
+
+        public FoodRepository(LocalDb localDb)
+            : base(localDb)
+        {
+        }
+    }
+}
