275d6684f31a59bacf2a15b8fef5e6e7fb393ed5
ddzibela <d.dzibela0000@gmail.com>
2022-12-09 01:34:16 +0100

    start work on detail page

---
+++ DameChales/DameChales.Web.App/Pages/Restaurant/RestaurantsDetailPage.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DameChales.Common.Models;
+using DameChales.Web.BL.Facades;
+using Microsoft.AspNetCore.Components;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DameChales.Web.App.Pages
+{
+    public partial class RestaurantsDetailPage
+    {
+        [Inject]
+        private NavigationManager navigationManager { get; set; } = null!;
+        [Inject]
+        private FoodFacade foodFacade { get; set; } = null!;
+        [Inject]
+        private RestaurantFacade restaurantFacade { get; set; } = null!;
+        private ICollection<FoodListModel> foods { get; set; } = new List<FoodListModel>();
+        private RestaurantDetailModel? restaurantDetailModel { get; set; }
+
+        [Parameter]
+        public Guid RestaurantID { get; set; } = Guid.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (RestaurantID == Guid.Empty)
+            {
+                navigationManager.NavigateTo($"/restaurants");
+            }
+            restaurantDetailModel = await restaurantFacade.GetByIdAsync( RestaurantID );
+            foods = await foodFacade.GetByRestaurantIdAsync( RestaurantID );
+
+            await base.OnInitializedAsync();
+        }
+
+    }
+}
