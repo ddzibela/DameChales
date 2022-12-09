6b6fc26c62b8809f295a207421290b5bd6c6cee6
ddzibela <d.dzibela0000@gmail.com>
2022-12-09 01:34:03 +0100

    List and Edit pages hopefully in working order, TODO add localizers

---
+++ DameChales/DameChales.Web.App/Pages/Restaurant/RestaurantsEditPage.razor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DameChales.Common.Models;
+using DameChales.Web.BL.Facades;
+using Microsoft.AspNetCore.Components;
+
+namespace DameChales.Web.App.Pages
+{
+    public partial class RestaurantsEditPage
+    {
+        [Inject]
+        private NavigationManager navigationManager { get; set; } = null!;
+        
+        [Inject]
+        private RestaurantFacade? RestaurantFacade { get; set; } = null;
+
+        private RestaurantDetailModel? restaurantDetailModel { get; set; } = GetNewRestaurantDetailModel();
+
+        [Parameter]
+        public Guid Id { get; init; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Id != Guid.Empty)
+            {
+                restaurantDetailModel = await RestaurantFacade!.GetByIdAsync(Id);
+            }
+            await base.OnInitializedAsync();
+        }
+
+        public async Task Save()
+        {
+            
+            await RestaurantFacade.SaveAsync(restaurantDetailModel);
+            navigationManager.NavigateTo($"/restaurants");
+        }
+
+        public async Task Delete()
+        {
+            await RestaurantFacade.DeleteAsync(Id);
+            navigationManager.NavigateTo($"/restaurants");
+        }
+
+        private static RestaurantDetailModel GetNewRestaurantDetailModel()
+            => new()
+            {
+                Id = Guid.Empty,
+                Address = string.Empty,
+                Description= string.Empty,
+                GPSCoordinates = string.Empty,
+                Name= string.Empty,
+                PhotoURL= string.Empty,
+                Foods = new List<FoodListModel>(),
+                Orders = new List<OrderListModel>()
+            };
+    }
+}
---
+++ DameChales/DameChales.Web.App/Pages/Restaurant/RestaurantsListPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DameChales.Common.Models;
+using DameChales.Web.BL.Facades;
+using Microsoft.AspNetCore.Components;
+
+namespace DameChales.Web.App.Pages
+{
+	public partial class RestaurantsListPage
+	{
+		[Inject]
+		private RestaurantFacade? facade { get; set; } = null;
+		private ICollection<RestaurantListModel> model = new List<RestaurantListModel>();
+		protected override async Task OnInitializedAsync()
+		{
+			model = await facade!.GetAllAsync();
+			await base.OnInitializedAsync();
+		}
+
+	}
+}
